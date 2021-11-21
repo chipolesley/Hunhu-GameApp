35842cf5140f30b8dd26cfef1a8ed8961488c58a
lesleychipo <lesley@qubedlab.com>
2021-11-21 22:33:53 +0200

    quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { QuizDataSource } from './quizDataSource';
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+public Questions: any = QuizDataSource;
+public countAnswers: number = 0
+public QuizTotal: number = 0;
+public Answers: Array<any> = [];
+public isFinished: boolean = false;
+
+  @ViewChild('f', { static: false })  formData!: NgForm;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {   
+  }
+
+  onSubmit(){    
+    this.countAnswers = 0; //to make sure that the count of correct answers is zero...
+    this.Answers = []; //making sure the array is empty on submission..
+
+    if(this.formData.touched == true && this.formData.valid == true){
+      this.isFinished = true;
+      //assign answers selected to the values array to iterate it...
+      this.Answers = Object.values(this.formData.value);
+      this.Answers.map(element =>{
+        //checking for the IDs of correct answers and having a counter count to have a final score
+        if(element == 1 || element == 6 ||element == 8 || element == 10 || element == 15) this.countAnswers++;
+      })
+      //getting the percentage of the mark
+      this.QuizTotal = (this.countAnswers / this.Answers.length) * 100;
+    }    
+  }
+  
+  onReset(){
+    //clear the form after the submit
+    this.formData.reset();
+    this.isFinished = false;
+    this.Answers = [];
+    this.QuizTotal = 0;
   }
 
 }
---
+++ src/app/quiz/quizDataSource.ts
@@ -0,0 +1,129 @@
+export const QuizDataSource =  [
+        {
+            id: 1001,
+            qtn:'What is the capital City of Zimbabwe?',
+            answers: [
+                {
+                    id: 1,
+                    text: 'Harare',
+                    whichQtn: "one"
+                },
+                {
+                    id: 2,
+                    text: 'Victoria Falls',
+                    whichQtn: "one"
+                    
+                },
+                {
+                    id: 3,
+                    text: 'Bulawayo',
+                    whichQtn: "one"
+                },
+                
+            ],
+            correctAns: {   
+                id: 1,
+                text: 'Harare' 
+            }
+        },
+        {
+            id: 1002,
+            qtn:'What is the capital City of Zambia?',
+            answers: [
+                {
+                    id: 4,
+                    text: 'Maramba',
+                    whichQtn: "two"
+                },               
+                {
+                    id: 5,
+                    text: 'Livingstone',
+                    whichQtn: "two"
+                },
+                {
+                    id: 6,
+                    text: 'Lusaka',
+                    whichQtn: "two"
+                },
+            ],
+            correctAns: {   
+                id: 6,
+                text: 'Lusaka' 
+            }
+        },
+        {
+            id: 1003,
+            qtn:'What is the capital City of Namibia?',
+            answers: [
+                {
+                    id: 7,
+                    text: 'Pretoria',
+                    whichQtn: "three"
+                },               
+                {
+                    id: 8,
+                    text: 'Windhoek',
+                    whichQtn: "three"
+                },
+                {
+                    id: 9,
+                    text: 'Zanzibar',
+                    whichQtn: "three"
+                },
+            ],
+            correctAns: {   
+                id: 8,
+                text: 'Windhoek' 
+            }
+        },
+        {
+            id: 1004,
+            qtn:'What is the capital City of Bostwana?',
+            answers: [
+                {
+                    id: 10,
+                    text: 'Gaborone',
+                    whichQtn: "four"
+                },               
+                {
+                    id: 11,
+                    text: 'Serowe',
+                    whichQtn: "four"
+                },
+                {
+                    id: 12,
+                    text: 'Francistown',
+                    whichQtn: "four"
+                },
+            ],
+            correctAns: {   
+                id: 10,
+                text: 'Gaborone' 
+            }
+        },
+        {
+            id: 1005,
+            qtn:'What is the capital City of Malawi?',
+            answers: [
+                {
+                    id: 13,
+                    text: 'Zomba',
+                    whichQtn: "five"
+                },               
+                {
+                    id: 14,
+                    text: 'Blantyre',
+                    whichQtn: "five"
+                },
+                {
+                    id: 15,
+                    text: 'Lilongwe',
+                    whichQtn: "five"
+                },
+            ],
+            correctAns: {   
+                id: 15,
+                text: 'Gaborone' 
+            }
+        }
+    ]
